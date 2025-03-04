f5eef5db58904627a25314e0e8d3fc7291a03d08
EhlOps <samtehlers@gmail.com>
2025-03-04 14:14:50 -0500

    Fix Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <>
       <footer className="py-5 text-center text-gray-500 text-xs">
-        <p>© {new Date().getFullYear()} John Doe. All rights reserved.</p>
+        <p>© {new Date().getFullYear()} Sam Ehlers. All rights reserved.</p>
       </footer>
     </>
   )
